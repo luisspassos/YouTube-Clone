89715d08e7c3432972e3f686986881f2071d03b9
Luís Passos <luis.passos013@gmail.com>
2021-12-09 21:18:13 -0300

    added animatiom of button tags

--- script.js
+++ script.js
@@ -2,6 +2,23 @@ const clearSearchBtn = document.querySelector("#clearSearch");
 const searchInput = document.querySelector(".searchInput input");
 const virtualKeyboard = document.querySelector("#virtualKeyboard");
 const form = document.querySelector("form");
+const nextBtn = document.querySelector(".nextBtn");
+const figureNextButton = document.querySelector(".nextBtn figure");
+
+function borderAnimation() {
+    figureNextButton.classList.add("borderNextBtn")
+    setTimeout(()=> {
+        figureNextButton.classList.remove("borderNextBtn")
+    }, 1000)
+}
+
+nextBtn.addEventListener("dragend", ()=> {
+    borderAnimation()
+})
+
+nextBtn.addEventListener("click", ()=> {
+    borderAnimation()
+})
 
 form.addEventListener("submit", (e)=> {
     e.preventDefault();
